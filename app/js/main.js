a2f645d4a52a18b42b305db86f7352c7fc99d9cf
alexbehma <alexbegma@gmail.com>
2019-10-30 11:51:38 +0200

    DOM; scss; img; animate.css

--- app/js/main.js
+++ app/js/main.js
@@ -5,13 +5,13 @@ $(function(){
 		fade: true,
 		prevArrow: '<img class="slider-arrow slider-arrow__left" src="img/arrow-left.svg" alt="arrow-left">',
 		nextArrow: '<img class="slider-arrow slider-arrow__right" src="img/arrow-right.svg" alt="arrow-right">',
-		asNavFor: '.slider-dots',
+		asNavFor: '.slider-dots'
 	});
 
 	$('.slider-dots').slick({
 		slidesToShow: 4,
 		slidesToScroll: 4,
-		asNavFor: '.header__slider',
+		asNavFor: '.header__slider'
 	});
 
 });
