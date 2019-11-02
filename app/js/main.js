12fc9390c0f47fd90283f32710336d6ba50ecdb7
alexbehma <alexbegma@gmail.com>
2019-11-02 19:28:33 +0200

    slider finished

--- app/js/main.js
+++ app/js/main.js
@@ -5,15 +5,20 @@ $(function(){
 		fade: true,
 		prevArrow: '<img class="slider-arrow slider-arrow__left" src="img/arrow-left.svg" alt="arrow-left">',
 		nextArrow: '<img class="slider-arrow slider-arrow__right" src="img/arrow-right.svg" alt="arrow-right">',
-		asNavFor: '.slider-dots'
+		asNavFor: '.slider-dots',
 	});
 
 	$('.slider-dots').slick({
 		slidesToShow: 4,
 		slidesToScroll: 4,
 		asNavFor: '.header__slider',
-		// centerMode: true,
-		// focusOnSelect: true
+	});
+
+	$('.surf-slider').slick({
+		prevArrow: '<img class="slider-arrow slider-arrow__left" src="img/arrow-left.svg" alt="arrow-left">',
+		nextArrow: '<img class="slider-arrow slider-arrow__right" src="img/arrow-right.svg" alt="arrow-right">',
+		slidesToShow: 4,
+		slidesToScroll: 1,
 	});
 
 });
