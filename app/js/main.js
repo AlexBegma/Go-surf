0b384589739b508e6bc090f2442c58cce12bbcce
alexbehma <alexbegma@gmail.com>
2020-01-25 14:21:29 +0200

    finished sleep-section from 25.01.2020

--- app/js/main.js
+++ app/js/main.js
@@ -31,4 +31,39 @@ $(function(){
 		focusOnSelect: true,
 	});
 
+	// CodePen Home input number custom style:
+
+	$('<div class="quantity-nav"><div class="quantity-button quantity-up"><img src="img/plus.png" alt="plus"></div><div class="quantity-button quantity-down"><img src="img/minus.png" alt="minus"></div></div>').insertAfter('.quantity input');
+	$('.quantity').each(function() {
+		var spinner = $(this),
+        input = spinner.find('input[type="number"]'),
+        btnUp = spinner.find('.quantity-up'),
+        btnDown = spinner.find('.quantity-down'),
+        min = input.attr('min'),
+        max = input.attr('max');
+
+      btnUp.click(function() {
+        var oldValue = parseFloat(input.val());
+        if (oldValue >= max) {
+          var newVal = oldValue;
+        } else {
+          var newVal = oldValue + 1;
+        }
+        spinner.find("input").val(newVal);
+        spinner.find("input").trigger("change");
+      });
+
+      btnDown.click(function() {
+        var oldValue = parseFloat(input.val());
+        if (oldValue <= min) {
+          var newVal = oldValue;
+        } else {
+          var newVal = oldValue - 1;
+        }
+        spinner.find("input").val(newVal);
+        spinner.find("input").trigger("change");
+      });
+
+    });
+
 });
